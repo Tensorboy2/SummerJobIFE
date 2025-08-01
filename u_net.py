3a9156560c472376ab8922b39fce777e8e27906f
Tensorboy2 <svargdal@online.no>
2025-08-01 10:39:16 +0200

    smaller batch size

--- u_net.py
+++ u_net.py
@@ -376,7 +376,7 @@ def get_dataloaders(config):
 def train_model():
     # Configuration
     config = {
-        'batch_size': 16,
+        'batch_size': 8,
         'val_ratio': 0.2,
         'num_workers': 0,
         'learning_rate': 8e-4,  # Lower learning rate
