9924de762bfaf18f8b7bf21ab8fd9ffe10ba12cc
Tensorboy2 <svargdal@online.no>
2025-08-01 09:19:22 +0200

    larger bs

--- u_net.py
+++ u_net.py
@@ -376,7 +376,7 @@ def get_dataloaders(config):
 def train_model():
     # Configuration
     config = {
-        'batch_size': 32,
+        'batch_size': 64,
         'val_ratio': 0.2,
         'num_workers': 0,
         'learning_rate': 2e-4,  # Lower learning rate
