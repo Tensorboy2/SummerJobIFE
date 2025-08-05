66267148ac286bf239004ccc2a3dd6692d0d5301
Tensorboy2 <svargdal@online.no>
2025-08-05 09:45:40 +0200

    anotherone

--- u_net.py
+++ u_net.py
@@ -408,7 +408,7 @@ def get_dataloaders(config):
 def train_model():
     # Configuration
     config = {
-        'batch_size': 32,
+        'batch_size': 64,
         'val_ratio': 0.2,
         'num_workers': 4,
         'learning_rate': 8e-4,  # Lower learning rate
@@ -431,7 +431,7 @@ def train_model():
     # Initialize model
     # model = ConvNeXtV2Segmentation(in_chans=12, num_classes=1, encoder_output_channels=320)
     # model = UNet(in_ch=12, out_ch=1)
-    model = create_convnextv3_segmentation(in_chans=12, num_classes=1, size='small')
+    model = create_convnextv3_segmentation(in_chans=12, num_classes=1, size='atto')
     device = torch.device(config['device'])
     model = model.to(device)
     
