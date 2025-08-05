d765dd096306f1b0582ea6eb6d1239d2d2e400b8
Tensorboy2 <svargdal@online.no>
2025-08-05 15:59:26 +0200

    test

--- u_net.py
+++ u_net.py
@@ -438,7 +438,7 @@ class CustomDataset(Dataset):
         #     mask = (mask - mask_min) / (mask_max - mask_min)
         # else:
         #     mask = torch.zeros_like(mask)
-        # mask = mask.float().clamp(0., 1.)
+        mask = mask.float().clamp(0., 1.)
 
 
         # Apply augmentations if training
